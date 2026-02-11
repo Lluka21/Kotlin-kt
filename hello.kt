fddb189af189438d7ed74158e06eb482c8f2f1f5
Luka Kartvelishvili <kartvelishvilil21@gmail.com>
2026-02-11 19:59:10 +0400

    string

--- hello.kt
+++ hello.kt
@@ -1,10 +1,6 @@
 fun main() {
- val a: UInt = 300u
- val b: UByte = 255u
- val c: ULong = 5000000000uL
+    val str = "abcd"
+    println(str.uppercase())
 
-    println(a)
-    println(b)
-    println(c)
-}
+}   
 
