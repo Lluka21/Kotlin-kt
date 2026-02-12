320d2af583a2e20ab97a6a68d4fbcd52ac6204e2
Luka Kartvelishvili <kartvelishvilil21@gmail.com>
2026-02-12 14:24:21 +0400

    when statement again

--- hello.kt
+++ hello.kt
@@ -1,10 +1,10 @@
 fun main() {
-   var x = 3
-   val text = when (x) {
-      1 -> println("x == 1")
-      2 -> println("x == 2")
-      else -> println("x is neither 1 nor 2")
-   }
+   val deliveryStatus = "OutForDelivery"
+   when(deliveryStatus) {
+      "Pending" -> print("Your order is being prepared")
+      "Shipped" -> print("Your order is on the way")
+      else -> print("Check your mini-map")
+   } 
    
     
 }
