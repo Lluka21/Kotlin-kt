0480993a1e27fb21e07cf3b0c1a1bb6f2ff1898c
Luka Kartvelishvili <kartvelishvilil21@gmail.com>
2026-02-12 15:21:33 +0400

    variable

--- hello.kt
+++ hello.kt
@@ -1,10 +1,8 @@
 fun main() {
-   val deliveryStatus = "OutForDelivery"
-   when(deliveryStatus) {
-      "Pending" -> print("Your order is being prepared")
-      "Shipped" -> print("Your order is on the way")
-      else -> print("Check your mini-map")
-   } 
+   val ticketPriority = "Medium"
+   when(ticketPriority) {
+      "Low", "Medium" -> print("Standart response time")
+      else -> print("High-priority handling")
+   }
    
-    
 }
