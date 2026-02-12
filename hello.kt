87014c0464282ad7059e79316fc479d5e083bebc
Luka Kartvelishvili <kartvelishvilil21@gmail.com>
2026-02-12 14:20:03 +0400

    when statement

--- hello.kt
+++ hello.kt
@@ -1,15 +1,10 @@
 fun main() {
-    val heightAlice = 160
-    val heightBob = 175
-
-    val taller = if(heightAlice > heightBob) {
-      println("Choose Alice\n")
-      heightAlice
-    } else {
-      println("Choose Bob\n")
-      heightBob
+   var x = 3
+   val text = when (x) {
+      1 -> println("x == 1")
+      2 -> println("x == 2")
+      else -> println("x is neither 1 nor 2")
    }
-
-   println("Taller height is $taller")
    
+    
 }
