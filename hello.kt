9bda2a2114dbfc45df03f7bebac46905c05884b7
Luka Kartvelishvili <kartvelishvilil21@gmail.com>
2026-02-14 23:58:59 +0400

    functions

--- hello.kt
+++ hello.kt
@@ -1,7 +1,18 @@
-fun <T> asList(vararg ts: T): List<T>{
-   val result = ArrayList<T>()
-   for(t in ts) {
-      result.add(t)
+fun main() {
+     val result = getMax(2.5, 3.7)
+     println(result)
    }
-   return result
+
+fun getMax(a: Int, b: Int): Int {
+   val max = if(a > b) a else b
+   return max
+
+}
+fun getMax(a: Double, b: Double): Double {
+   val max = if(a > b) a else b
+   return max
+}
+fun getMax(a: Double, b: Double): Double {
+   val max = if(a > b) a else b
+   return max
 }
