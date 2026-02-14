fb0937065a70f01b16b5782adb7150fc4116ade1
Luka Kartvelishvili <kartvelishvilil21@gmail.com>
2026-02-14 22:07:43 +0400

    vararg

--- hello.kt
+++ hello.kt
@@ -1,14 +1,7 @@
-fun printHello(name:String?, action:() -> Unit) {
-   if(name !== null) {
-      println("Hello $name")
-   } else {
-      println("Hi there")
+fun <T> asList(vararg ts: T): List<T>{
+   val result = ArrayList<T>()
+   for(t in ts) {
+      result.add(t)
    }
-   action()
+   return result
 }
-
-
-fun main(){ 
-   printHello("Kodee"){}
-}
-
