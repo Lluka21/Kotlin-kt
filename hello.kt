c91723f5a4924362b81e04e378b02ee8f661b8ec
Luka Kartvelishvili <155449325+Lluka21@users.noreply.github.com>
2026-02-16 17:12:50 +0400

    Update hello.kt

--- hello.kt
+++ hello.kt
@@ -2,7 +2,7 @@
 fun <T> asList(vararg ts: T): List<T>{
    val result = ArrayList<T>()
    for(t in ts) {
-      result.add(
+      result.a
 
 fun main() {
    val fruits = listOf("Apple","Banana","Cherry")
