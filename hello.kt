b1bb17ad1b7582c12aff4f2b25a462145d720329
Luka Kartvelishvili <kartvelishvilil21@gmail.com>
2026-02-21 19:44:34 +0400

    new file

--- hello.kt
+++ hello.kt
@@ -1,24 +1,5 @@
 fun main() {
-   var number = 1
-   val lastNumber = 20
-   var evenNumberCounter = 0
 
-   while(number <= lastNumber) {
-      number++
-
-      if(isEvenNumber(number)) {
-         continue
-      }
-
-      println(number)
-   }
 
 }
 
-fun isEvenNumber(number: Int): Boolean {
-   if((number % 2 ) == 0) {
-      return true
-   } else {
-      return false
-   }
-}
