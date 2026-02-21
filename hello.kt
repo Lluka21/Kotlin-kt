04d54a6d33666ac0f63f65dab2eb9aa56a0c38fe
Luka Kartvelishvili <kartvelishvilil21@gmail.com>
2026-02-21 18:31:11 +0400

    commit

--- hello.kt
+++ hello.kt
@@ -1,18 +1,11 @@
 fun main() {
-     val result = getMax(2.5, 3.7)
-     println(result)
+
+   for(i in 1 until 10) {
+      println(i)
    }
 
-fun getMax(a: Int, b: Int): Int {
-   val max = if(a > b) a else b
-   return max
+}
 
-}
-fun getMax(a: Double, b: Double): Double {
-   val max = if(a > b) a else b
-   return max
-}
-fun getMax(a: Double, b: Double): Double {
-   val max = if(a > b) a else b
-   return max
-}
+  
+
+
