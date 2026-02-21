a67f065343ee745cb908da1288015c9ac881a7e0
Luka Kartvelishvili <kartvelishvilil21@gmail.com>
2026-02-21 19:33:10 +0400

    loop challenge

--- hello.kt
+++ hello.kt
@@ -1,10 +1,24 @@
 fun main() {
-   var number = 0
+   var number = 1
+   val lastNumber = 20
+   var evenNumberCounter = 0
 
-   while(number < 10) {
+   while(number <= lastNumber) {
       number++
+
+      if(isEvenNumber(number)) {
+         continue
+      }
 
       println(number)
    }
+
 }
 
+fun isEvenNumber(number: Int): Boolean {
+   if((number % 2 ) == 0) {
+      return true
+   } else {
+      return false
+   }
+}
