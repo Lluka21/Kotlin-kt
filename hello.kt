95629d1f5ca6559aec4a4e661d84d90d76485f13
Luka Kartvelishvili <155449325+Lluka21@users.noreply.github.com>
2026-08-13 21:30:59 +0400

    Update hello.kt

--- hello.kt
+++ hello.kt
@@ -1,32 +1,6 @@
 
 fun main() {
-   var number = 1
-   val lastNumber = 20
-   var evenNumberCounter = 0
 
-   while(number <= lastNumber) {
-      number++
 
-      if(isEvenNumber(number)) {
-         continue
-      }
-      
-      println(number)
-
-fun <T> asList(vararg ts: T): List<T>{
-   val result = ArrayList
    
-fun main() {
-   val fruits = listOf("Apple","Banana","Cherry")
-
-   for((index,fruit) in fruits.withIndex()) {
-      println("$index: $ruit")
-   }
-}
-fun isEvenNumber(number: Int): Boolean {
-   if((number % 2 ) == 0) {
-      return true
-   } else {
-      return false
-   }
 }
